4190a03426084f77fe4d475a5ba85d419ad50799
Dane Danovskis <DaneD319@gmail.com>
2021-12-07 16:03:53 -0500

    Added the styling on the login and register pages

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,17 +2,26 @@
   <div class="home">
     <div class="beerFlags">
       <beer-type-flag v-for="beerType in this.$store.state.beerType" v-bind:key="beerType" v-bind="beerType"></beer-type-flag>
+      <noise-flag></noise-flag>
+      <money-flag></money-flag>
+      <brewery-card v-for="brewery in this.$store.state.breweryList" v-bind:key="brewery"></brewery-card>
     </div>
   </div>
 </template>
 
 <script>
 import BeerTypeFlag from '@/components/BeerTypeFlag.vue'
+import NoiseFlag from '@/components/NoiseFlag.vue'
+import MoneyFlag from '@/components/MoneyFlag.vue'
+import BreweryCard from '@/components/BreweryCard.vue'
 
 export default {
   name: "home",
   components:{
-    BeerTypeFlag
+    BeerTypeFlag,
+    NoiseFlag,
+    MoneyFlag,
+    BreweryCard
   }
 };
 </script>
@@ -22,6 +31,7 @@ export default {
 .home{
   display: grid;
   grid-template-rows: 1fr 1fr 6fr;
+  grid-template-areas: "beers searches breweries";
   padding: .5em;
 }
 
@@ -30,6 +40,7 @@ export default {
   flex-direction: row;
   align-content: center;
   justify-content: space-around;
+  grid-area: "beers";
 }
 
 </style>
